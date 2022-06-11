cc65b80438d21fbddccfd462a0d0d7e559b26ecb
Derek Cordova <derekooocordova@gmail.com>
2022-06-11 10:06:47 -0400

    Added updated Method

--- src/test/java/javaClass.java
+++ src/test/java/javaClass.java
@@ -1,5 +1,11 @@
 public class javaClass {
     public static void main(String[] args) {
         System.out.println("This is Derek");
+
+        System.out.println("Updated Method");
+
     }
+
+
+
 }
